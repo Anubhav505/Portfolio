0407eda4d1ce84f7a10832685877ad7d8d4b9f27
Anubhav <xcaliber704@gmail.com>
2025-09-20 03:02:13 +0530

    updated project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -34,36 +34,7 @@ export default function Projects() {
         </ul>
       </div>
 
-      {/* Vandelyn Project */}
-      <div className="hover:bg-[rgba(255,255,255,0.1)] py-3 px-3 rounded-md group space-y-2">
-        <a
-          className="flex items-center group-hover:items-start group-hover:text-[#58ffcf] text-lg font-semibold"
-          href="https://vandelyn.com/"
-          target="_blank"
-        >
-          VANDELYN – Lifestyle eCommerce Platform &nbsp;
-          <ArrowUpRight size={16} />
-        </a>
-        <p className="text-gray-400 text-justify text-[1rem]">
-          Built a <strong>lifestyle eCommerce platform</strong> for a brand 
-          offering a wide product range beyond fashion.  
-          Designed an intuitive <strong>category system</strong> for browsing, 
-          integrated <strong>secure checkout & tracking</strong>, and 
-          customized the admin dashboard for bulk product management.  
-          Focused on a clean, minimal UI optimized for performance and 
-          scalability to handle growing traffic.
-        </p>
-        <ul className="flex flex-wrap gap-2 text-xs font-semibold">
-          <li className="skill">JavaScript</li>
-          <li className="skill">TypeScript</li>
-          <li className="skill">Next.js</li>
-          <li className="skill">Tailwind CSS</li>
-          <li className="skill">GSAP</li>
-          <li className="skill">Mongoose</li>
-          <li className="skill">Razorpay</li>
-          <li className="skill">Embla Carousel</li>
-        </ul>
-      </div>
+      <div className="h-96"></div>
 
       <div className="h-96"></div>
     </div>
